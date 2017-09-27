2ade3da8c4303cde5dd4ac73f279ee33568de10d
Winter LaMon <winter.lamon@gmail.com>
2017-09-27 03:38:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    return array.join(" and ")
+  else array.count >= 3
+    array[0...-1].join(", ") + ", and " + array[-1]
+  end
 end
